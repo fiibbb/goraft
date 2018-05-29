5a77f27cf4ca582b1e0db3e29c9122a30b98a493
Ling Yuan <lingy@uber.com>
2018-05-29 11:12:13 -0700

    Readability refactor

--- raft/handler.go
+++ raft/handler.go
@@ -8,68 +8,64 @@ import (
 
 // RequestVote simply forwards the requests to the Raft process so they can be handled in serial.
 func (n *Node) RequestVote(ctx context.Context, req *pb.RequestVoteRequest) (*pb.RequestVoteResponse, error) {
-	respChan := make(chan *pb.RequestVoteResponse)
-	errChan := make(chan error)
-	n.requestVoteChan <- &requestVoteArg{
+	arg := &requestVoteArg{
 		req:      req,
-		respChan: respChan,
-		errChan:  errChan,
+		respChan: make(chan *pb.RequestVoteResponse),
+		errChan:  make(chan error),
 	}
+	n.requestVoteChan <- arg
 	select {
-	case resp := <-respChan:
+	case resp := <-arg.respChan:
 		return resp, nil
-	case err := <-errChan:
+	case err := <-arg.errChan:
 		return nil, err
 	}
 }
 
 // AppendEntries simply forwards the requests to the Raft process so they can be handled in serial.
 func (n *Node) AppendEntries(ctx context.Context, req *pb.AppendEntriesRequest) (*pb.AppendEntriesResponse, error) {
-	respChan := make(chan *pb.AppendEntriesResponse)
-	errChan := make(chan error)
-	n.appendEntriesChan <- &appendEntriesArg{
+	arg := &appendEntriesArg{
 		req:      req,
-		respChan: respChan,
-		errChan:  errChan,
+		respChan: make(chan *pb.AppendEntriesResponse),
+		errChan:  make(chan error),
 	}
+	n.appendEntriesChan <- arg
 	select {
-	case resp := <-respChan:
+	case resp := <-arg.respChan:
 		return resp, nil
-	case err := <-errChan:
+	case err := <-arg.errChan:
 		return nil, err
 	}
 }
 
 // Write simply forwards the requests to the Raft process so they can be handled in serial.
 func (n *Node) Write(ctx context.Context, req *pb.WriteRequest) (*pb.WriteResponse, error) {
-	respChan := make(chan *pb.WriteResponse)
-	errChan := make(chan error)
-	n.writeChan <- &writeArg{
+	arg := &writeArg{
 		req:      req,
-		respChan: respChan,
-		errChan:  errChan,
+		respChan: make(chan *pb.WriteResponse),
+		errChan:  make(chan error),
 	}
+	n.writeChan <- arg
 	select {
-	case resp := <-respChan:
+	case resp := <-arg.respChan:
 		return resp, nil
-	case err := <-errChan:
+	case err := <-arg.errChan:
 		return nil, err
 	}
 }
 
 // DumpState simply forwards the requests to the Raft process so they can be handled in serial.
 func (n *Node) DumpState(ctx context.Context, req *pb.DumpStateRequest) (*pb.DumpStateResponse, error) {
-	respChan := make(chan *pb.DumpStateResponse)
-	errChan := make(chan error)
-	n.dumpStateChan <- &dumpStateArg{
+	arg := &dumpStateArg{
 		req:      req,
-		respChan: respChan,
-		errChan:  errChan,
+		respChan: make(chan *pb.DumpStateResponse),
+		errChan:  make(chan error),
 	}
+	n.dumpStateChan <- arg
 	select {
-	case resp := <-respChan:
+	case resp := <-arg.respChan:
 		return resp, nil
-	case err := <-errChan:
+	case err := <-arg.errChan:
 		return nil, err
 	}
 }
